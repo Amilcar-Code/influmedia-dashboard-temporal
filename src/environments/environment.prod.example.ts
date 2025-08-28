e051001befcf5fe0fbf866cb8955840472abe4ac
Amilcar-Code <amilcar.xicay@code.com.gt>
2025-08-28 09:38:21 -0600

    chore: ignore real envs and add example env files

---
+++ src/environments/environment.prod.example.ts
@@ -0,0 +1,12 @@
+// environment.example.ts
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'REEMPLAZAR',
+    authDomain: 'REEMPLAZAR',
+    projectId: 'REEMPLAZAR',
+    storageBucket: 'REEMPLAZAR',
+    messagingSenderId: 'REEMPLAZAR',
+    appId: 'REEMPLAZAR',
+  },
+};
