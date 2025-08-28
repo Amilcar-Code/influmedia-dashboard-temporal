af423da3b19c8fa3d3a4f69b5cea606f1134f5f1
Amilcar-Code <amilcar.xicay@code.com.gt>
2025-08-28 00:42:18 -0600

    enviroment update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,15 +1,16 @@
+// src/environments/environment.prod.ts
 export const environment = {
+  production: true,
   firebase: {
-    apiKey: 'AIzaSyCPm3GH5_wwBZqInfwL-bzFFw3b_erto2I',
+    apiKey: 'AIzaSyCPm3GHS_uwBZqInfwL-bzfFw3b_erto2I',
     authDomain: 'influmedia-automation.firebaseapp.com',
     projectId: 'influmedia-automation',
-    storageBucket: 'influmedia-automation.firebasestorage.app',
-    messagingSenderId: '725141753279',
-    appId: '1:725141753279:web:5a10b8b434588765cb650a',
+    storageBucket: 'influmedia-automation.appspot.com',
+    messagingSenderId: '172514753279',
+    appId: '1:172514753279:web:5a10b8b434588765cb650a',
   },
   testAuth: {
     email: 'tester@influmedia.dev',
     password: '123456',
   },
-  production: false,
 };
